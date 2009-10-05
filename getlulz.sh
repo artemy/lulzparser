5cd01e6d9ef72fbea43c5974c781e3afdc35b53f
Artemy M <artimac@gmail.com>
2009-10-05 17:55:00 +0400

    there is scripts, muzafuka

---
+++ getlulz.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+MYFILE=`date +%H%M%S`
+mkdir $LOLDATE
+for i in `seq -w 1 $1`
+do
+python getlulz.py $2$i.html | grep -E '.(png|jpg|jpeg|pdf|gif)$' >> $LOLDATE/wget-this-file-$LOLDATE
+echo $i of $1
+done
