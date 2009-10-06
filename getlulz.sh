75d14b87bfb6499591e8e2123d6b6496848e446a
Nikolay Edigaryev <edigaryev@gmail.com>
2009-10-06 09:27:49 +0600

    getluls.sh no more needs

--- getlulz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-MYFILE=`date +%H%M%S`
-mkdir $LOLDATE
-for i in `seq -w 1 $1`
-do
-python getlulz.py $2$i.html | grep -E '.(png|jpg|jpeg|pdf|gif)$' >> $LOLDATE/wget-this-file-$LOLDATE
-echo $i of $1
-done
